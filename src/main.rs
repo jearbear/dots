8b9808cf5a56dd652cc531a88cda9a5cf08459ec
Jerry Tegno <jerrytegno@gmail.com>
2018-07-07 18:05:08 -0700

    refactor using Dotfile struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate clap;
+#[macro_use]
+extern crate lazy_static;
 extern crate walkdir;
 
 use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
@@ -9,17 +11,16 @@ use std::{env, fs};
 
 static INFO: &str = "dots - Dotfile management made less toilesome.";
 
+lazy_static! {
+    static ref HOME_DIR: PathBuf = env::home_dir().unwrap();
+    static ref DOT_ROOT: PathBuf = HOME_DIR.join(".dotfiles");
+}
+
 type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
 fn main() -> Result<()> {
-    let list_command = SubCommand::with_name("list")
-        .about("List all installed dotfiles in the current directory")
-        .arg(
-            Arg::with_name("source")
-                .help("Directory to list")
-                .long("source")
-                .takes_value(true),
-        );
+    let list_command =
+        SubCommand::with_name("list").about("List all installed dotfiles in the current directory");
 
     let add_command = SubCommand::with_name("add")
         .about("Link dotfile to home directory")
@@ -41,47 +42,29 @@ fn main() -> Result<()> {
 
     match matches.subcommand_name() {
         Some("add") => add(matches.subcommand_matches("add").unwrap()),
-        Some("list") => list(matches.subcommand_matches("list").unwrap()),
+        Some("list") => list(),
         _ => Ok(()),
     }
 }
 
-fn add(matches: &ArgMatches) -> Result<()> {
+fn add(_matches: &ArgMatches) -> Result<()> {
     Ok(())
 }
 
-fn list(matches: &ArgMatches) -> Result<()> {
-    let home_dir = env::home_dir().unwrap();
-    let home_dir_str = home_dir.to_str().unwrap();
+fn list() -> Result<()> {
+    println!("Installed dotfiles from {}:", DOT_ROOT.to_string_lossy());
 
-    let source_dir = match matches.value_of("source") {
-        Some(dir) => PathBuf::from(dir),
-        None => home_dir.join(".dotfiles"),
-    };
-    let source_dir_str = source_dir.to_str().unwrap();
-
-    println!("Installed dotfiles from {}:", source_dir_str);
-
-    WalkDir::new(&source_dir)
+    let entries = WalkDir::new(DOT_ROOT.to_path_buf())
         .into_iter()
         .filter_entry(|e| !is_ignored(e))
-        .filter_map(|e| e.ok())
-        .for_each(|e| {
-            let stripped_source_path = e.path()
-                .strip_prefix(&source_dir)
-                .unwrap()
-                .to_str()
-                .unwrap();
+        .filter_map(|e| e.ok());
 
-            let installed_path_str = format!("{}/.{}", home_dir_str, stripped_source_path);
-            let installed_path = PathBuf::from(installed_path_str);
-
-            if let Ok(source_path) = fs::read_link(installed_path) {
-                if source_path == e.path() {
-                    println!("- {}", stripped_source_path);
-                }
-            }
-        });
+    for e in entries {
+        let dotfile = Dotfile::new(e.path())?;
+        if dotfile.installed() {
+            println!("- {}", dotfile.path_str());
+        }
+    }
 
     Ok(())
 }
@@ -92,4 +75,37 @@ fn is_ignored(entry: &DirEntry) -> bool {
         .to_str()
         .map(|s| s.starts_with(".git"))
         .unwrap_or(false)
+}
+
+struct Dotfile {
+    path: PathBuf,
+}
+
+impl Dotfile {
+    fn new<P: Into<PathBuf>>(path: P) -> Result<Dotfile> {
+        Ok(Dotfile {
+            path: path.into()
+                .strip_prefix(DOT_ROOT.to_path_buf())?
+                .to_path_buf(),
+        })
+    }
+
+    fn path_str(&self) -> String {
+        self.path.to_string_lossy().to_string()
+    }
+
+    fn source(&self) -> PathBuf {
+        DOT_ROOT.join(&self.path)
+    }
+
+    fn target(&self) -> PathBuf {
+        HOME_DIR.join(PathBuf::from(format!(".{}", self.path.to_string_lossy())))
+    }
+
+    fn installed(&self) -> bool {
+        match fs::read_link(self.target()) {
+            Ok(linked) => linked == self.source(),
+            _ => false,
+        }
+    }
 }
