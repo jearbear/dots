c266879920a9110450fc091023b9ab059eb7daa4
Jerry Tegno <jerry@stripe.com>
2019-01-19 00:24:36 -0800

    simplify argument parsing

--- src/args.rs
+++ src/args.rs
@@ -1,137 +1,117 @@
-use std::ffi::{OsStr, OsString};
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-use structopt::clap::AppSettings;
-use structopt::StructOpt;
+use clap::{App, AppSettings, Arg, SubCommand};
 
-#[derive(StructOpt, Debug)]
-#[structopt(
-    name = "dots - Dotfile management made less toilesome.",
-    about = "",
-    author = "",
-    version = "",
-    raw(global_setting = "AppSettings::DisableVersion")
-)]
-pub struct Opt {
-    #[structopt(
-        name = "store",
-        help = "Path of dotfile store [default: ~/.dotfiles]",
-        long = "store",
-        raw(global = "true"),
-        value_name = "PATH",
-        parse(try_from_os_str = "resolve_dir")
-    )]
-    pub store: Option<PathBuf>,
+use crate::dotfile::Store;
+use crate::error::{AppError, Result};
 
-    #[structopt(subcommand)]
-    pub cmd: Command,
+pub struct Args {
+    pub store: Store,
+    pub command: Command,
 }
 
-#[derive(StructOpt, Debug)]
 pub enum Command {
-    #[structopt(
-        name = "install",
-        about = "Link dotfile(s) to target in home directory",
-        author = "",
-        version = ""
-    )]
-    Install {
-        #[structopt(
-            help = "Dotfile(s) to install",
-            parse(try_from_os_str = "resolve_file"),
-            raw(required = "true")
-        )]
-        dotfiles: Vec<PathBuf>,
-    },
-
-    #[structopt(
-        name = "uninstall",
-        about = "Unlink dotfile(s) from target in home directory",
-        author = "",
-        version = ""
-    )]
-    Uninstall {
-        #[structopt(
-            help = "Dotfile(s) to uninstall",
-            parse(try_from_os_str = "resolve_file"),
-            raw(required = "true")
-        )]
-        dotfiles: Vec<PathBuf>,
-    },
-
-    #[structopt(
-        name = "manage",
-        about = "Move dotfile(s) to the store and link them back to their target",
-        author = "",
-        version = ""
-    )]
-    Manage {
-        #[structopt(
-            help = "Dotfile(s) to manage",
-            parse(try_from_os_str = "resolve_file"),
-            raw(required = "true")
-        )]
-        dotfiles: Vec<PathBuf>,
-    },
-
-    #[structopt(
-        name = "unmanage",
-        about = "Move dotfile(s) out of the store and back to their target",
-        author = "",
-        version = ""
-    )]
-    Unmanage {
-        #[structopt(
-            help = "Dotfile(s) to unmanage",
-            parse(try_from_os_str = "resolve_file"),
-            raw(required = "true")
-        )]
-        dotfiles: Vec<PathBuf>,
-    },
-
-    #[structopt(
-        name = "list",
-        about = "List all installed dotfiles in the given store",
-        author = "",
-        version = ""
-    )]
-    List {},
+    List,
+    Install(PathBuf),
+    Uninstall(PathBuf),
+    Manage(PathBuf),
+    Unmanage(PathBuf),
 }
 
-fn resolve_path(path: &OsStr) -> Result<PathBuf, OsString> {
-    let path = fs::canonicalize(path).map_err(|_| {
-        OsString::from(format!(
-            "Path `{}` could not be resolved.",
-            path.to_string_lossy()
+pub fn init() -> Result<Args> {
+    let store = Arg::with_name("store")
+        .help("Path of dotfile store [default: ~/.dotfiles]")
+        .long("store")
+        .value_name("PATH")
+        .global(true);
+
+    let dotfiles = Arg::with_name("dotfiles")
+        .help("Dotfile(s) to manage")
+        .value_name("DOTFILE")
+        .multiple(true)
+        .required(true);
+
+    let list =
+        SubCommand::with_name("list").about("List all installed dotfiles in the given store");
+
+    let install = SubCommand::with_name("install")
+        .about("Link dotfile(s) to target in home directory")
+        .arg(&dotfiles);
+
+    let uninstall = SubCommand::with_name("uninstall")
+        .about("Unlink dotfile(s) to target in home directory")
+        .arg(&dotfiles);
+
+    let manage = SubCommand::with_name("manage")
+        .about("Move dotfile(s) to the store and link them back to their target")
+        .arg(&dotfiles);
+
+    let unmanage = SubCommand::with_name("unmanage")
+        .about("Move dotfile(s) out of the store and back to their target")
+        .arg(&dotfiles);
+
+    let matches = App::new("dots - Dotfile management made less toilesome.")
+        .arg(&store)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .global_setting(AppSettings::DisableVersion)
+        .subcommands(vec![list, install, uninstall, manage, unmanage])
+        .get_matches();
+
+    let store = resolve_store(&matches)?;
+
+    let command = match matches.subcommand() {
+        ("list", _) => Command::List,
+        ("install", Some(sub_m)) => Command::Install(resolve_dotfile(sub_m)?),
+        ("uninstall", Some(sub_m)) => Command::Uninstall(resolve_dotfile(sub_m)?),
+        ("manage", Some(sub_m)) => Command::Manage(resolve_dotfile(sub_m)?),
+        ("unmanage", Some(sub_m)) => Command::Unmanage(resolve_dotfile(sub_m)?),
+        _ => unreachable!(),
+    };
+
+    Ok(Args { store, command })
+}
+
+fn resolve_store(matches: &clap::ArgMatches) -> Result<Store> {
+    let store_path = matches
+        .value_of("store")
+        .map(PathBuf::from)
+        .unwrap_or_else(|| dirs::home_dir().unwrap().join(".dotfiles"));
+
+    let store_path = resolve_path(&store_path)?;
+
+    if store_path.is_dir() {
+        Ok(Store::new(&store_path))
+    } else {
+        AppError::result(&format!(
+            "Path `{}` is not a directory.",
+            store_path.display()
         ))
-    })?;
+    }
+}
+
+fn resolve_dotfile(matches: &clap::ArgMatches) -> Result<PathBuf> {
+    let dotfile_path = matches.value_of("dotfile").map(PathBuf::from).unwrap();
+    let dotfile_path = resolve_path(&dotfile_path)?;
+
+    let is_reg_file = dotfile_path
+        .metadata()
+        .map(|m| m.is_file())
+        .unwrap_or(false);
+
+    if is_reg_file {
+        Ok(dotfile_path)
+    } else {
+        AppError::result(&format!(
+            "Path `{}` is not a regular file.",
+            dotfile_path.display()
+        ))
+    }
+}
+
+fn resolve_path(path: &Path) -> Result<PathBuf> {
+    let path = fs::canonicalize(path)
+        .map_err(|_| AppError::new(&format!("Path `{}` could not be resolved.", path.display())))?;
 
     Ok(path)
-}
-
-fn resolve_dir(path: &OsStr) -> Result<PathBuf, OsString> {
-    let path = resolve_path(path)?;
-
-    if path.is_dir() {
-        Ok(path)
-    } else {
-        Err(OsString::from(format!(
-            "Path `{}` must be a directory.",
-            path.display()
-        )))
-    }
-}
-
-fn resolve_file(path: &OsStr) -> Result<PathBuf, OsString> {
-    let path = resolve_path(path)?;
-
-    if path.is_file() {
-        Ok(path)
-    } else {
-        Err(OsString::from(format!(
-            "Path `{}` must be a file.",
-            path.display()
-        )))
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,90 +2,72 @@ mod args;
 mod dotfile;
 mod error;
 
-use std::fs;
 use std::path::{Path, PathBuf};
 
-use structopt::StructOpt;
-
-use crate::args::{Command, Opt};
+use crate::args::Command;
 use crate::dotfile::{DFState, Dotfile, Store};
 use crate::error::{AppError, Result};
 
-fn do_main(args: Opt) -> Result<()> {
-    let home_dir = dirs::home_dir().unwrap();
-    let dot_root = args.store.unwrap_or_else(|| home_dir.join(".dotfiles"));
+fn main() {
+    let res = args::init().map(|mut args| match args.command {
+        Command::List => list_dotfiles(&args.store),
+        Command::Install(df) => install_dotfile(&args.store, &df),
+        Command::Uninstall(df) => uninstall_dotfile(&args.store, &df),
+        Command::Manage(df) => manage_dotfile(&mut args.store, &df),
+        Command::Unmanage(df) => unmanage_dotfile(&mut args.store, &df),
+    });
 
-    if !fs::metadata(&dot_root)?.is_dir() {
+    if let Err(err) = res {
+        eprintln!("Error: {}", err);
+        std::process::exit(1);
+    }
+}
+
+fn install_dotfile(store: &Store, path: &PathBuf) -> Result<()> {
+    let dotfile = fetch_dotfile(store, path)?;
+
+    match dotfile.state() {
+        DFState::Installed => Ok(()),
+        DFState::Blocked => AppError::result(&format!(
+            "Dotfile target `{}` is blocked.",
+            dotfile.target.display()
+        )),
+        DFState::Uninstalled => dotfile.install(),
+    }?;
+
+    Ok(())
+}
+
+fn uninstall_dotfile(store: &Store, path: &PathBuf) -> Result<()> {
+    let dotfile = fetch_dotfile(store, path)?;
+
+    match dotfile.state() {
+        DFState::Installed => dotfile.uninstall(),
+        DFState::Blocked | DFState::Uninstalled => Ok(()),
+    }?;
+
+    Ok(())
+}
+
+fn manage_dotfile(store: &mut Store, target: &PathBuf) -> Result<()> {
+    if store.get(target).is_some() {
         return AppError::result(&format!(
-            "Given store path `{} is not a directory.",
-            dot_root.display()
+            "Dotfile with target `{}` already exists in the store.",
+            target.display()
         ));
     }
 
-    let mut store = Store::new(&dot_root);
-
-    match args.cmd {
-        Command::Install { dotfiles } => install_dotfiles(&store, &dotfiles),
-        Command::Uninstall { dotfiles } => uninstall_dotfiles(&store, &dotfiles),
-        Command::Manage { dotfiles } => manage_dotfiles(&mut store, &dotfiles),
-        Command::Unmanage { dotfiles } => unmanage_dotfiles(&mut store, &dotfiles),
-        Command::List {} => list_dotfiles(&store),
-    }
-}
-
-fn install_dotfiles(store: &Store, paths: &[PathBuf]) -> Result<()> {
-    for path in paths {
-        let dotfile = fetch_dotfile(store, path)?;
-
-        match dotfile.state() {
-            DFState::Installed => Ok(()),
-            DFState::Blocked => AppError::result(&format!(
-                "Dotfile target `{}` is blocked.",
-                dotfile.target.display()
-            )),
-            DFState::Uninstalled => dotfile.install(),
-        }?;
-    }
+    Dotfile::from_target(&store.path, target)
+        .and_then(|df| df.store().map(|_| df))
+        .map(|df| store.add(df))?;
 
     Ok(())
 }
 
-fn uninstall_dotfiles(store: &Store, paths: &[PathBuf]) -> Result<()> {
-    for path in paths {
-        let dotfile = fetch_dotfile(store, path)?;
-
-        match dotfile.state() {
-            DFState::Installed => dotfile.uninstall(),
-            DFState::Blocked | DFState::Uninstalled => Ok(()),
-        }?;
-    }
-
-    Ok(())
-}
-
-fn manage_dotfiles(store: &mut Store, targets: &[PathBuf]) -> Result<()> {
-    for target in targets {
-        if store.get(target).is_some() {
-            return AppError::result(&format!(
-                "Dotfile with target `{}` already exists in the store.",
-                target.display()
-            ));
-        }
-
-        Dotfile::from_target(&store.path, target)
-            .and_then(|df| df.store().map(|_| df))
-            .map(|df| store.add(df))?;
-    }
-
-    Ok(())
-}
-
-fn unmanage_dotfiles(store: &mut Store, paths: &[PathBuf]) -> Result<()> {
-    for path in paths {
-        let dotfile = fetch_dotfile(store, path)?;
-        dotfile.unstore()?;
-        store.remove(path);
-    }
+fn unmanage_dotfile(store: &mut Store, path: &PathBuf) -> Result<()> {
+    let dotfile = fetch_dotfile(store, path)?;
+    dotfile.unstore()?;
+    store.remove(path);
 
     Ok(())
 }
@@ -116,14 +98,5 @@ fn fetch_dotfile<'a>(store: &'a Store, path: &Path) -> Result<&'a Dotfile> {
             "Dotfile not found with reference `{}`.",
             path.display()
         )),
-    }
-}
-
-fn main() {
-    let args = Opt::from_args();
-
-    if let Err(err) = do_main(args) {
-        eprintln!("Error: {}", err);
-        std::process::exit(1);
     }
 }
